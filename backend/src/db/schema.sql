71a1efe7b0a32e2f896ea7b01a64b8510512fae0
hosurVijay <hosurvijayendra@gmail.com>
2026-09-07 00:46:16 +0530

    email indexed

--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -5,7 +5,7 @@ USE wound_tracker;
 CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
     phoneNumber VARCHAR(10) NOT NULL,
     password_hashed VARCHAR(255) NOT NULL,
     profileImage VARCHAR(500),
